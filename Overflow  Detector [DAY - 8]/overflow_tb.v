2976ea9e0cda418ad4c5b0c6b7c02fc0a49f8683
Saikiran12346 <121762168+Saikiran12346@users.noreply.github.com>
2023-01-30 23:10:49 +0530

    Create overflow_tb.v

---
+++ Overflow  Detector [DAY - 8]/overflow_tb.v
@@ -0,0 +1,28 @@
+module overflow_tb();
+
+reg [3:0]a,b;
+reg cin;
+wire [3:0]sum;
+wire cout;
+wire of;
+
+overflow dut(.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout), .of(of));
+
+initial 
+begin       
+{a,b,cin} = 0;
+#10;
+a = 4'b1111; b = 4'b1000; cin = 0;
+ #10;
+a = 4'b0111; b = 4'b0100; cin = 1'b0;
+ #10;
+a = 4'b0111; b = 4'b1101; cin = 1'b0;
+ #10;  
+end
+
+initial
+begin
+$monitor(" a=%b, b = %b, cin=%b ,sum=%b, cout=%b ,of=%b ", a, b, cin, sum, cout, of);
+end
+
+endmodule
