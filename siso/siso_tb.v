a6a47e1d9b8f96f6d0be9e09e6004932f0a6a0ea
Saikiran12346 <121762168+Saikiran12346@users.noreply.github.com>
2023-03-10 23:42:42 +0530

    Create siso_tb.v

---
+++ siso/siso_tb.v
@@ -0,0 +1,22 @@
+module testbench();
+reg d,clk;
+wire q;
+siso a(d, clk, q);
+initial
+begin
+$monitor($time,"d=%b,clk=%b,q=%b",d,clk,q);
+end
+initial
+begin
+clk=1'b0;
+forever #5 clk=~clk;
+end
+initial
+begin
+d=1;
+#10 d=0;
+#10 d=1;
+#10 d=1;
+#40 $finish;
+end 
+endmodule
